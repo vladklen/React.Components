fbe22af2281aa5a03e958d16f481269696f7ac6a
vladklen <Vlad6097944@gmail.com>
2023-10-29 14:59:49 +0300

    refactor: husky added

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-shadow */
+/* eslint-disable react/button-has-type */
 import { useState } from 'react';
 import reactLogo from './assets/react.svg';
 import viteLogo from '../../../../../../../../vite.svg';
