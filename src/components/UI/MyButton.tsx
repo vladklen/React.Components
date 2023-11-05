9e19f3e3f59e14a4cd9d2f03264bc59d649fe945
vladklen <Vlad6097944@gmail.com>
2023-11-05 13:20:03 +0300

    init: React.Routing start task

--- src/components/UI/MyButton.tsx
+++ src/components/UI/MyButton.tsx
@@ -7,29 +7,10 @@ interface IButtonProps {
   click: React.MouseEventHandler<HTMLButtonElement>;
 }
 
-interface IButtonState {
-  buttonText: string;
-  clickHandler: React.MouseEventHandler<HTMLButtonElement>;
+export default function MyButton({ message, color, click }: IButtonProps) {
+  return (
+    <StyledButton type="button" $inputColor={color} onClick={click}>
+      {message}{' '}
+    </StyledButton>
+  );
 }
-
-class MyButton extends React.Component<IButtonProps, IButtonState> {
-  constructor(props: IButtonProps) {
-    super(props);
-    this.state = {
-      buttonText: props.message,
-      clickHandler: props.click,
-    };
-  }
-
-  render(): JSX.Element {
-    const { color } = this.props;
-    const { clickHandler, buttonText } = this.state;
-    return (
-      <StyledButton type="button" $inputColor={color} onClick={clickHandler}>
-        {buttonText}{' '}
-      </StyledButton>
-    );
-  }
-}
-
-export default MyButton;
