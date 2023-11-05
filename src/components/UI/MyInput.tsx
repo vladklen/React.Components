100cca654ea77460f8d20377c937daed2a470f29
vladklen <Vlad6097944@gmail.com>
2023-11-05 15:04:20 +0300

    feat: class components changed to functional

--- src/components/UI/MyInput.tsx
+++ src/components/UI/MyInput.tsx
@@ -1,16 +1,11 @@
 import React, { ChangeEventHandler } from 'react';
 import { StyledInput } from './Styles';
 
-interface IInputState {
+interface IInputProps {
   message: string;
   change: ChangeEventHandler<HTMLInputElement>;
 }
 
-class MyInput extends React.PureComponent<IInputState> {
-  render(): JSX.Element {
-    const { message, change } = this.props;
-    return <StyledInput type="text" defaultValue={message} onChange={change} />;
-  }
+export default function MyInput({ message, change }: IInputProps) {
+  return <StyledInput type="text" defaultValue={message} onChange={change} />;
 }
-
-export default MyInput;
--- src/components/Сard.tsx
+++ src/components/Сard.tsx
@@ -1,7 +1,6 @@
-import React from 'react';
 import { StyledCard } from './UI/Styles';
 
-export interface CardProps {
+interface CardProps {
   birth_year: string;
   name: string;
   height: string;
@@ -9,20 +8,32 @@ export interface CardProps {
   gender: string;
 }
 
-class Card extends React.PureComponent<CardProps> {
-  render(): JSX.Element {
-    // eslint-disable-next-line @typescript-eslint/naming-convention
-    const { name, birth_year, height, mass, gender } = this.props;
-    return (
-      <StyledCard>
-        <h3>Name:{name}</h3>
-        <p>BirthDate:{birth_year}</p>
-        <p>Height:{height}</p>
-        <p>Mass:{mass}</p>
-        <p>Gender:{gender}</p>
-      </StyledCard>
-    );
-  }
+function Card({ birth_year, name, height, mass, gender }: CardProps) {
+  return (
+    <StyledCard>
+      <h3>Name:{name}</h3>
+      <p>BirthDate:{birth_year}</p>
+      <p>Height:{height}</p>
+      <p>Mass:{mass}</p>
+      <p>Gender:{gender}</p>
+    </StyledCard>
+  );
 }
 
+// class Card extends React.PureComponent<CardProps> {
+//   render(): JSX.Element {
+//     // eslint-disable-next-line @typescript-eslint/naming-convention
+//     const { name, birth_year, height, mass, gender } = this.props;
+//     return (
+//       <StyledCard>
+//         <h3>Name:{name}</h3>
+//         <p>BirthDate:{birth_year}</p>
+//         <p>Height:{height}</p>
+//         <p>Mass:{mass}</p>
+//         <p>Gender:{gender}</p>
+//       </StyledCard>
+//     );
+//   }
+// }
 export { Card };
+export type { CardProps };
