4273866cb60aa98aba158f15f37e7c33d3065258
vladklen <Vlad6097944@gmail.com>
2023-11-20 01:44:34 +0300

    feat: test add for Card

--- tests/components/Card/Card.test.tsx
+++ tests/components/Card/Card.test.tsx
@@ -1,102 +1,97 @@
-// import { describe, expect, test, vi } from 'vitest';
-// import { act, fireEvent, render } from '@testing-library/react';
-// import { MemoryRouter, Route, Routes } from 'react-router-dom';
-// import Home from '../../../src/pages/Home';
-// import { data, pagination } from '../../mocks/AnimeRespone';
-// import AnimeContextProvider from '../../../src/context/Context';
-// import PersonDetails from '../../../src/components/PersonalCard/PersonDetails';
-// import NotFound from '../../../src/pages/NotFound';
-// import { getAnimeById } from '../../../src/api/StartSearch';
+import { describe, expect, test, vi } from 'vitest';
+import { act, fireEvent, render } from '@testing-library/react';
+import { MemoryRouter, Route, Routes } from 'react-router-dom';
+import Home from '../../../src/pages/Home';
+import { data, pagination } from '../../mocks/AnimeRespone';
+import PersonDetails from '../../../src/components/PersonalCard/PersonDetails';
+import NotFound from '../../../src/pages/NotFound';
+import { useGetCardByIdQuery } from '../../../src/store/animeApi';
 
-// vi.mock('../../../src/api/StartSearch', () => {
-//   return {
-//     getAnime: vi.fn(() => Promise.resolve({ data, pagination })),
-//     getAnimeById: vi.fn(() => Promise.resolve({ data: data[0] })),
-//   };
-// });
+const mockState = {
+  value: {
+    search: 'test',
+    page: '1',
+    limit: '10',
+  },
+};
 
-// describe('Test Card component', () => {
-//   test('Test Home component render', async () => {
-//     const wrapper = render(
-//       <AnimeContextProvider>
-//         <MemoryRouter>
-//           <Home />
-//         </MemoryRouter>
-//       </AnimeContextProvider>
-//     );
+vi.mock('react-redux', () => ({
+  useDispatch: () => vi.fn(),
+  useSelector: vi.fn(() => mockState),
+}));
 
-//     const items = await wrapper.findAllByRole('link');
+vi.mock('../../../src/store/animeApi', () => ({
+  useGetCardListQuery: vi.fn(() => ({
+    isLoading: false,
+    data: { data, pagination },
+  })),
+  useGetCardByIdQuery: vi.fn(() => ({ isLoading: false, data: data[0] })),
+}));
 
-//     expect(items.length).toBe(3);
-//   });
+describe('Test Card component', () => {
+  test('Test Home component render', async () => {
+    const wrapper = render(
+      <MemoryRouter>
+        <Home />
+      </MemoryRouter>
+    );
 
-//   test('Ensure that the card component renders the relevant card data;', async () => {
-//     const wrapper = render(
-//       <AnimeContextProvider>
-//         <MemoryRouter>
-//           <Home />
-//         </MemoryRouter>
-//       </AnimeContextProvider>
-//     );
+    const items = await wrapper.findAllByRole('link');
 
-//     const items = await wrapper.findAllByRole('link');
+    expect(items.length).toBe(3);
+  });
 
-//     expect(items[0]).toMatchSnapshot();
-//   });
+  test('Ensure that the card component renders the relevant card data;', async () => {
+    const wrapper = render(
+      <MemoryRouter>
+        <Home />
+      </MemoryRouter>
+    );
 
-//   test('Validate that clicking on a card opens a detailed card component', async () => {
-//     const wrapper = render(
-//       <MemoryRouter initialEntries={['/']}>
-//         <Routes>
-//           <Route
-//             path="/"
-//             element={
-//               <AnimeContextProvider>
-//                 <Home />
-//               </AnimeContextProvider>
-//             }
-//           >
-//             <Route path="details/:id" element={<PersonDetails />} />
-//           </Route>
-//           <Route path="*" element={<NotFound />} />
-//         </Routes>
-//       </MemoryRouter>
-//     );
-//     const linkElement = await wrapper.findByTestId(`test${data[0].mal_id}`);
+    const items = await wrapper.findAllByRole('link');
 
-//     act(() => {
-//       fireEvent.click(linkElement);
-//     });
+    expect(items[0]).toMatchSnapshot();
+  });
 
-//     expect(
-//       await wrapper.findByTestId(`cardDetails${data[0].mal_id}`)
-//     ).toBeTruthy();
-//   });
+  test('Validate that clicking on a card opens a detailed card component', async () => {
+    const wrapper = render(
+      <MemoryRouter initialEntries={['/']}>
+        <Routes>
+          <Route path="/" element={<Home />}>
+            <Route path="details/:id" element={<PersonDetails />} />
+          </Route>
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </MemoryRouter>
+    );
+    const linkElement = await wrapper.findByTestId(`test${data[0].mal_id}`);
 
-//   test('Check that clicking triggers an additional API call to fetch detailed information', async () => {
-//     const wrapper = render(
-//       <MemoryRouter initialEntries={['/']}>
-//         <Routes>
-//           <Route
-//             path="/"
-//             element={
-//               <AnimeContextProvider>
-//                 <Home />
-//               </AnimeContextProvider>
-//             }
-//           >
-//             <Route path="details/:id" element={<PersonDetails />} />
-//           </Route>
-//           <Route path="*" element={<NotFound />} />
-//         </Routes>
-//       </MemoryRouter>
-//     );
-//     const linkElement = await wrapper.findByTestId(`test${data[0].mal_id}`);
+    act(() => {
+      fireEvent.click(linkElement);
+    });
 
-//     act(() => {
-//       fireEvent.click(linkElement);
-//     });
+    expect(
+      await wrapper.findByTestId(`cardDetails${data[0].mal_id}`)
+    ).toBeTruthy();
+  });
 
-//     expect(getAnimeById).toBeCalled();
-//   });
-// });
+  test('Check that clicking triggers an additional API call to fetch detailed information', async () => {
+    const wrapper = render(
+      <MemoryRouter initialEntries={['/']}>
+        <Routes>
+          <Route path="/" element={<Home />}>
+            <Route path="details/:id" element={<PersonDetails />} />
+          </Route>
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </MemoryRouter>
+    );
+    const linkElement = await wrapper.findByTestId(`test${data[0].mal_id}`);
+
+    act(() => {
+      fireEvent.click(linkElement);
+    });
+
+    expect(useGetCardByIdQuery).toBeCalled();
+  });
+});
